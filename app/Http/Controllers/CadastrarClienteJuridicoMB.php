f82280965c868ce5060718fc25cafe28c20317c8
Felipe <felipeserbim@hotmail.com>
2018-06-14 18:08:12 -0300

    correção do bug da tela branca

--- app/Http/Controllers/CadastrarClienteJuridicoMB.php
+++ app/Http/Controllers/CadastrarClienteJuridicoMB.php
@@ -12,14 +12,7 @@ class CadastrarClienteJuridicoMB extends Controller
     }
 
     public function CadastroJ(Request $request){
-        if($request->nomeFantasia !=null && $request->cep !=null && $request->areaAtuacao !=null
-        && $request->telefone !=null && $request->cnpj !=null && $request->razaoSocial !=null
-        && $request->cidade !=null && $request->rua !=null && $request->n !=null){
-         $estancia=CadastrarClienteJuridicoMB::valida($request); 
-        }   
-    }
 
-    public function valida(Request $request){
         $request->validate([
             'cep' => 'numeric',
             'cnpj' => 'numeric',
@@ -37,7 +30,9 @@ class CadastrarClienteJuridicoMB extends Controller
         $clienteJ->rua = $request->rua;
         $clienteJ->numero = $request->n;    
         $clienteJ->save();   
-        return view ('clientes.cadastrarClienteJuridico');
+        return redirect()->route('cadClienteJuridico');  
+         //$estancia=CadastrarClienteJuridicoMB::valida($request); 
+        
     }
     
 }
